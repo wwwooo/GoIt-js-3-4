d72651f7ecc9dda195a61590edef14edc346ff45
wwwooo <malinovsvska@gmail.com>
2016-08-22 08:29:48 +0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,109 +1,88 @@
 var create = {
-    element: function(options) {
+    form: function() {
+        this.createElement({
+            tag: 'form'
+        });
+    },
+    h1: function() {
+        this.createElement({
+            tag: 'h1',
+            parent: 'form',
+            text: 'Тест по программированию'
+        });
+    },
+    ol: function() {
+        this.createElement({
+            tag: 'ol',
+            parent: 'form'
+        });
+    },
+    countLi: 1,
+    li: function() {
+        this.createElement({
+            tag: 'li',
+            className: 'li-' + this.countLi,
+            parent: 'ol'
+        });
+        this.createElement({
+            tag: 'span',
+            parent: 'li-' + this.countLi,
+            text: 'Вопрос №' + this.countLi
+        });
+
+        for (var i = 1; i <= 3; i++) {
+            this.createElement({
+                tag: 'input',
+                parent: 'li-' + this.countLi,
+                attributs: {
+                    type: 'radio',
+                    id: 'radio-' + this.countLi + '-' + i,
+                    name: 'radio-' + this.countLi
+                }
+            });
+            this.createElement({
+                tag: 'label',
+                className: 'label',
+                parent: 'li-' + this.countLi,
+                attributs: {
+                    for: 'radio-' + this.countLi + '-' + i,
+                },
+                text: 'Вариант ответа №' + i
+            });
+        }
+        this.countLi++;
+    },
+    input: function() {
+        this.createElement({
+            tag: 'input',
+            parent: 'form',
+            className: 'submit',
+            attributs: {
+                type: 'submit',
+                value: 'Проверить мои результаты'
+            }
+        });
+    },
+    createElement: function(options) {
         var parentElem = (options.parent) ?  document.querySelector('.' + options.parent) : document.body;
         var elem = document.createElement(options.tag);
 
+        elem.classList.add(options.tag);
+        (options.className) ? elem.classList.add(options.className) : '';
         if (options.attributs) {
             for (var key in options.attributs) {
                 elem.setAttribute(key, options.attributs[key]);
             }
         }
-        elem.classList.add(options.tag);
-        (options.className) ? elem.classList.add(options.className) : '';
         elem.innerHTML = options.text || '';
         parentElem.appendChild(elem);
     }
 };
 
-create.element({
-    tag: 'h1',
-    text: 'Тест по программированию'
-});
-create.element({
-    tag: 'form',
-});
-create.element({
-    tag: 'ol',
-    parent: 'form'
-});
-
-create.element({
-    tag: 'li',
-    className: 'li-1',
-    parent: 'ol'
-});
-create.element({
-    tag: 'li',
-    className: 'li-2',
-    parent: 'ol'
-});
-create.element({
-    tag: 'li',
-    className: 'li-3',
-    parent: 'ol'
-});
-
-create.element({
-    tag: 'span',
-    parent: 'li-1',
-    text: 'Вопрос №1'
-});
-create.element({
-    tag: 'span',
-    parent: 'li-2',
-    text: 'Вопрос №2'
-});
-create.element({
-    tag: 'span',
-    parent: 'li-3',
-    text: 'Вопрос №3'
-});
-/*---------------------------li-1 options----------------------------------*/
-create.element({
-    tag: 'input',
-    parent: 'li-1',
-    attributs: {
-        type: 'radio',
-        id: 'optradio-1-1',
-        name: 'optradio-1'
-    }
-});
-create.element({
-    tag: 'label',
-    className: 'label',
-    parent: 'li-1',
-    attributs: {
-        for: 'optradio-1-1'
-    },
-    text: 'Ответ №1'
-});
-create.element({
-    tag: 'input',
-    parent: 'li-1',
-    attributs: {
-        type: 'radio',
-        id: 'optradio-1-2',
-        name: 'optradio-1'
-    }
-});
-create.element({
-    tag: 'label',
-    className: 'label',
-    parent: 'li-1',
-    attributs: {
-        for: 'optradio-1-2'
-    },
-    text: 'Ответ №2'
-});
-/*---------------------------li-2 options----------------------------------*/
-create.element({
-    tag: 'label',
-    parent: 'li-2',
-    text: 'Ответ №2'
-});
-/*---------------------------li-3 options----------------------------------*/
-create.element({
-    tag: 'label',
-    parent: 'li-3',
-    text: 'Ответ №3'
-});
+create.form();
+create.h1();
+create.ol();
+create.li();
+create.li();
+create.li();
+create.input();
