2e35924c4abb5da319ac26ccc02370e7f4949a4f
wwwooo <malinovsvska@gmail.com>
2016-08-25 11:24:05 +0300

    update main.js: fragment

--- js/main.js
+++ js/main.js
@@ -4,14 +4,16 @@ var addAttrs = function(obj, attributes) {
     }
 };
 
-var createElem = function(tag, parent, text) {
+var createElem = function(tag, parent, text, attrs) {
     var elem = document.createElement(tag);
     elem.innerHTML = text || '';
+    addAttrs(elem, attrs);
     return parent.appendChild(elem);
 };
 
-var form = createElem('form', document.body);
-var h1 = createElem('h1', form, 'Тест по программированию');
+var fragment = document.createDocumentFragment();
+var h1 = createElem('h1', fragment, 'Тест по программированию');
+var form = createElem('form', fragment);
 var ol = createElem('ol', form);
 
 for (var i = 1; i <= 3; i++) {
@@ -20,11 +22,11 @@ for (var i = 1; i <= 3; i++) {
 
     for (var n = 1; n <= 3; n++) {
         var label = createElem('label', li, 'Вариант ответа №' + n);
-        var radio = createElem('input', label);
-        addAttrs(radio, {'type': 'radio', 'name': 'radio-' + i});
+        var radio = createElem('input', label, null, {'type': 'radio', 'name': 'radio-' + i});
         var span = createElem('span', label);
     }
 }
 
-var submit = createElem('input', form);
-addAttrs(submit, {type: 'submit', value: 'Проверить мои результаты'});
+var submit = createElem('input', form, null, {type: 'submit', value: 'Проверить мои результаты'});
+
+document.body.appendChild(fragment);
