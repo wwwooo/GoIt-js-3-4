af333cf635f6f533b4011ca44778b213e49ba82e
wwwooo <malinovsvska@gmail.com>
2016-08-25 10:13:12 +0300

    update main.js and style.css

--- js/main.js
+++ js/main.js
@@ -1,65 +1,30 @@
-var setAttributes = function(attributs) {
-    for (var key in attributs) {
-        this.setAttribute(key, attributs[key]);
+var addAttrs = function(obj, attributes) {
+    for (var key in attributes) {
+        obj.setAttribute(key, attributes[key]);
     }
 };
 
 var createElem = function(tag, parent, text) {
     var elem = document.createElement(tag);
     elem.innerHTML = text || '';
-    parent.appendChild(elem).addAttrb = setAttributes;
-    return elem;
+    return parent.appendChild(elem);
 };
 
 var form = createElem('form', document.body);
 var h1 = createElem('h1', form, 'Тест по программированию');
 var ol = createElem('ol', form);
-var li = createElem('li', ol);
-var span = createElem('span', li, 'Вопрос №1');
-var radio = createElem('input', li)
-radio.addAttrb({'type': 'radio', 'id': 'radio-1-1', 'name': 'radio-1'});
-var label = createElem('label', li, 'Вариант ответа №1');
-label.addAttrb({for: 'radio-1-1'});
-var radio = createElem('input', li);
-radio.addAttrb({'type': 'radio', 'id': 'radio-1-2', 'name': 'radio-1'});
-var label = createElem('label', li, 'Вариант ответа №2');
-label.addAttrb({for: 'radio-1-2'});
-var radio = createElem('input', li);
-radio.addAttrb({type: 'radio', id: 'radio-1-3', name: 'radio-1'});
-var label = createElem('label', li, 'Вариант ответа №3');
-label.addAttrb({for: 'radio-1-3'});
 
-var li = createElem('li', ol);
-var span = createElem('span', li, 'Вопрос №2');
-var radio = createElem('input', li);
-radio.addAttrb({type: 'radio', id: 'radio-2-1', name: 'radio-2'});
-var label = createElem('label', li, 'Вариант ответа №1');
-label.addAttrb({for: 'radio-2-1'});
-var radio = createElem('input', li);
-radio.addAttrb({type: 'radio', id: 'radio-2-2', name: 'radio-2'});
-var label = createElem('label', li, 'Вариант ответа №2');
-label.addAttrb({for: 'radio-2-2'});
-var radio = createElem('input', li);
-radio.addAttrb({type: 'radio', id: 'radio-2-3', name: 'radio-2'});
-var label = createElem('label', li, 'Вариант ответа №3');
-label.addAttrb({for: 'radio-2-3'});
+for (var i = 1; i <= 3; i++) {
+    var li = createElem('li', ol);
+    var span = createElem('span', li, 'Вопрос №' + i);
 
-var li = createElem('li', ol);
-var span = createElem('span', li, 'Вопрос №3');
-var radio = createElem('input', li);
-radio.addAttrb({type: 'radio', id: 'radio-3-1', name: 'radio-3'});
-var label = createElem('label', li, 'Вариант ответа №1');
-label.addAttrb({for: 'radio-3-1'});
-
-var radio = createElem('input', li);
-radio.addAttrb({type: 'radio', id: 'radio-3-2', name: 'radio-3'});
-var label = createElem('label', li, 'Вариант ответа №2');
-label.addAttrb({for: 'radio-3-2'});
-
-var radio = createElem('input', li);
-radio.addAttrb({type: 'radio', id: 'radio-3-3', name: 'radio-3'});
-var label = createElem('label', li, 'Вариант ответа №3');
-label.addAttrb({for: 'radio-3-3'});
+    for (var n = 1; n <= 3; n++) {
+        var label = createElem('label', li, 'Вариант ответа №' + n);
+        var radio = createElem('input', label);
+        addAttrs(radio, {'type': 'radio', 'name': 'radio-' + i});
+        var span = createElem('span', label);
+    }
+}
 
 var submit = createElem('input', form);
-submit.addAttrb({type: 'submit', value: 'Проверить мои результаты'});
+addAttrs(submit, {type: 'submit', value: 'Проверить мои результаты'});
